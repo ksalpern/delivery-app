5f613ef920f431d9f2eb6790352416b7e5b58330
AzAlex <azalexxx02@gmail.com>
2022-07-07 10:16:02 +0300

    add cart info to initial state

--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,8 +1,10 @@
-import { fetchUser } from "../utils/fetchLocalStorageData";
+import { fetchCart, fetchUser } from "../utils/fetchLocalStorageData";
 
 const userInfo = fetchUser();
+const cartInfo = fetchCart();
 
 export const initialState = {
   user: userInfo,
   productItems: null,
+  cartItems: cartInfo
 };
